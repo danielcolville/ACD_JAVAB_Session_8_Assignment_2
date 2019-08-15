be43f84e905f0930852d9208dfe85f77466336fb
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-14 22:39:28 -0400

    Add files via upload

---
+++ PrimeThread.java
@@ -0,0 +1,28 @@
+package session8;
+
+public class PrimeThread extends Thread implements Runnable {
+	
+	int num;
+	PrimeThread(int num) {
+		this.num=num;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		int breakpoint=num/2;
+		if(num==0||num==1) {
+			System.out.println(num+" is not Prime");
+			return;
+		}
+		else {
+			for(int i=2;i<=breakpoint;i++) {
+				if(num%i==0) {
+					
+					return;
+				}
+			}
+		}
+		System.out.println(num+" is prime");
+	}
+}
---
+++ PrimeThreadMain.java
@@ -0,0 +1,13 @@
+package session8;
+
+public class PrimeThreadMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrimeThread pt1=new PrimeThread(37);
+		PrimeThread pt2=new PrimeThread(55);
+		pt1.start();
+		pt2.start();
+	}
+
+}
